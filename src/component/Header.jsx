0488396d37ee215ee761a4ee21265fbf4cf8132e
mrDavilBoy <singhsawan320@gmail.com>
2025-07-06 23:15:02 +0530

    ho gaye

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,14 +5,17 @@ export default function Header() {
   return (
    <>
    <div className='bg-slate-200 w-full '>
-        <div className='w-[90%] mx-auto bg-amber-300'>
+        <div className='w-[90%] mx-auto border border-black flex items-center'>
            
            {/* logo section */}
             <div className='w-[8%] border border-amber-500'>
                 <img src={logo} alt="logo" className="w-[100px] h-[50px]"/>
             </div>
 
-            
+            <div className='w-[5%] border-amber-500 font-bold text-[20px] border-b-black border-2'>
+               Other
+            </div>
+
         </div>
    </div>
    </>
