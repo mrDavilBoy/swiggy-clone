69d1ede4dbc5a571023b1e0410403c393baf6ba7
mrDavilBoy <singhsawan320@gmail.com>
2025-07-06 23:28:34 +0530

    DOne again

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,15 +4,15 @@ import logo from '../assets/logo.png'
 export default function Header() {
   return (
    <>
-   <div className='bg-slate-200 w-full '>
-        <div className='w-[90%] mx-auto border border-black flex items-center'>
+   <div className='bg-slate-200 w-full'>
+        <div className='w-[90%] mx-auto  flex items-center'>
            
            {/* logo section */}
-            <div className='w-[8%] border border-amber-500'>
-                <img src={logo} alt="logo" className="w-[100px] h-[50px]"/>
+            <div className='w-[8%] '>
+                <img src={logo} alt="logo" className="w-[100px] h-[50px] hover:w-[110px] hover:h-[55px]"/>
             </div>
 
-            <div className='w-[5%] border-amber-500 font-bold text-[20px] border-b-black border-2'>
+            <div className='w-[5%] font-bold text-[20px] border-b-black border-b-2'>
                Other
             </div>
 
