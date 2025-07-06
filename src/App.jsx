4941ef89b45f5d77404881da428a48f43377bd63
mrDavilBoy <singhsawan320@gmail.com>
2025-07-06 21:41:32 +0530

    done 1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './component/Header'
 
 export default function App() {
   return (
     <>
     <div>
       <h1 className='text-red-800'>hello i am shavan </h1>
+      <Header />
     </div>
     </>
   )
---
+++ src/component/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className='bg-amber-500'>bye shavan </h1>
+    </div>
+  )
+}
